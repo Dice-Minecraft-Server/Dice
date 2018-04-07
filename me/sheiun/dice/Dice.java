f4f81c5072e5d27a2de51abc67507d22faf2a479
SheiUn <d215039@outlook.com>
2018-04-07 12:24:18 +0800

    First push

---
+++ me/sheiun/dice/Dice.java
@@ -0,0 +1,78 @@
+package me.sheiun.dice;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.logging.Level;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author SheiUn
+ */
+public class Dice extends JavaPlugin {
+
+    public Map<UUID, Dicer> dicers = new HashMap<UUID, Dicer>();
+    public PlayerListener pListener;
+
+    @Override
+    public void onEnable() {
+        pListener = new PlayerListener(this);
+
+        // 儲存資料
+        saveData();
+
+        // 讀取資料
+        loadData();
+
+        // 初始化
+        getServer().getPluginManager().registerEvents(pListener, this);
+
+        getLogger().log(Level.INFO, "已啟用!");
+    }
+
+    @Override
+    public void onDisable() {
+
+        getLogger().log(Level.INFO, "已停用!");
+    }
+
+    public void saveData() {
+        // save Dicers data to data.yml
+
+    }
+
+    public void loadData() {
+        YamlConfiguration config = new YamlConfiguration();
+        File dataFile = new File(this.getDataFolder(), "data.yml");
+        try {
+            config.load(dataFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Dicer getDicer(UUID id) {
+        Dicer dicer = null;
+        if (dicers.containsKey(id)) {
+            dicer = dicers.get(id);
+        }
+        if (dicer == null) {
+            dicer = new Dicer(this);
+            dicer.id = id;
+            if (dicer.loadData() == true) {
+                dicers.put(id, dicer);
+            }
+        }
+        return dicer;
+    }
+}
---
+++ me/sheiun/dice/quest/Quest.java
@@ -0,0 +1,9 @@
+package me.sheiun.dice.quest;
+
+/**
+ *
+ * @author SheiUn
+ */
+public class Quest {
+    
+}
